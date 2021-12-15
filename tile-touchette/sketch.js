ae7e29134b11e4d61f6d8bb8ee77d70ccae8e49e
jkenzer <jkenzer@gmail.com>
2021-12-15 08:18:07 -0700

    Fixed tile. Added grid.

--- tile-touchette/sketch.js
+++ tile-touchette/sketch.js
@@ -1,5 +1,7 @@
+let tileWidth = 100;
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(820, 820);
   angleMode(DEGREES);
   rectMode(CENTER);
   noLoop();
@@ -9,23 +11,35 @@ function setup() {
 }
 
 function draw() {
-  drawTile(10, 10, 180);
-  drawTile(110, 10, 90);
-  drawTile(210, 10, 0);
-  drawTile(10, 110, 180);
-  drawTile(110, 110, 90);
-  drawTile(210, 110, 90);
+  for (let x = 5; x < width + 15; x += 105) {
+    for (let y = 5; y < height + 15; y += 105) {
+      drawTile(x, y, Math.random() < 0.5 ? 0 : 90);
+    }
+  }
 }
 
 function drawTile(x, y, a) {
-  let tileWidth = 100;
   push();
   translate(x + tileWidth / 2, y + tileWidth / 2);
   rotate(a);
   square(0, 0, 100);
-  arc((-1 * tileWidth) / 2, (-1 * tileWidth) / 2, 55, 55, 0, 90);
-  arc((-1 * tileWidth) / 2, (-1 * tileWidth) / 2, 85, 85, 0, 90);
-  arc(tileWidth / 2, tileWidth / 2, 55, 55, 180, 270);
-  arc(tileWidth / 2, tileWidth / 2, 85, 85, 180, 270);
+  arc(
+    (-1 * tileWidth) / 2,
+    (-1 * tileWidth) / 2,
+    tileWidth - 15,
+    tileWidth - 15,
+    0,
+    90
+  );
+  arc(
+    (-1 * tileWidth) / 2,
+    (-1 * tileWidth) / 2,
+    tileWidth + 15,
+    tileWidth + 15,
+    0,
+    90
+  );
+  arc(tileWidth / 2, tileWidth / 2, tileWidth - 15, tileWidth - 15, 180, 270);
+  arc(tileWidth / 2, tileWidth / 2, tileWidth + 15, tileWidth + 15, 180, 270);
   pop();
 }
