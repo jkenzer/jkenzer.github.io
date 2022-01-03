9d525cf4c572ad0a746cc9b8a2d766b39e9d744b
jkenzer <jkenzer@gmail.com>
2022-01-03 15:46:10 -0700

    added rings

--- genuary-2022-3-space/sketch.js
+++ genuary-2022-3-space/sketch.js
@@ -12,15 +12,22 @@ function draw() {
   for (let index = 90; index < 275; index = index + 3) {
     x = r * cos(index);
     y = r * sin(index);
-    circle(x, y, 3);
-    circle(x - 10, y, 3);
-    line(x, y, x - 10, y);
+    circle(x, y, 1);
+    circle(x - 14, y, 1);
+    line(x, y, x - 14, y);
   }
   for (let index = 275; index < 450; index = index + 3) {
     x = r * cos(index);
     y = r * sin(index);
-    circle(x, y, 3);
-    circle(x + 10, y, 3);
+    circle(x, y, 1);
+    circle(x + 10, y, 1);
     line(x, y, x + 10, y);
   }
+  rotate(-8);
+  arc(0, 0, 310, 60, 349, 190);
+  arc(0, 0, 320, 70, 346, 193);
+
+  arc(0, 0, 350, 100, 339, 200);
+  arc(0, 0, 360, 110, 335, 203);
+  arc(0, 0, 370, 120, 332, 206);
 }
