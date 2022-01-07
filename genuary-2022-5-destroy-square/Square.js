aa9bfde659698ecf18f108b15820e1a98c86f790
jkenzer <jkenzer@gmail.com>
2022-01-06 17:09:47 -0700

    Finally got what I wanted

--- genuary-2022-5-destroy-square/Square.js
+++ genuary-2022-5-destroy-square/Square.js
@@ -5,19 +5,21 @@ class Square {
     this.size = size;
     this.res = res;
     this.points = [];
-    this.breakPointRadius = 1;
+    this.breakPointRadius = 2;
     this.mult = 0.5;
     this.numSteps = this.size + this.size / 2;
     let density = 20;
 
     this.breakPoint = createVector(random(size), random(size));
-    for (let angle = 0; angle < 360; angle += 60) {
-      let x = cos(angle) * this.breakPointRadius;
-      let y = sin(angle) * this.breakPointRadius;
-      let p = createVector(x, y);
-
+    for (let angle = 0; angle < 360; angle += 15) {
+      let p = createVector(0, 1);
+      p.setHeading(angle);
+      p.mult(this.breakPointRadius);
       p.add(this.breakPoint);
-      this.points.push(p);
+      this.points.push({
+        p,
+        heading: angle,
+      });
     }
   }
 
@@ -25,33 +27,31 @@ class Square {
     push();
     translate(this.x, this.y);
     rect(0, 0, this.size, this.size);
-    // circle(this.breakPoint.x, this.breakPoint.y, this.breakPointRadius);
+    circle(this.breakPoint.x, this.breakPoint.y, this.breakPointRadius + 5);
     for (let i = 0; i < this.points.length; i++) {
+      beginShape();
       for (let step = 0; step < this.numSteps; step++) {
         let angle;
         const p = this.points[i];
-        if (p.x < this.breakPoint.x && p.y < this.breakPoint.y) {
-          angle = map(noise(p.x * this.mult, p.y * this.mult), 0, 1, 180, 270);
-        } else if (p.x > this.breakPoint.x && p.y < this.breakPoint.y) {
-          angle = map(noise(p.x * this.mult, p.y * this.mult), 0, 1, 270, 360);
-        } else if (p.x < this.breakPoint.x && p.y > this.breakPoint.y) {
-          angle = map(noise(p.x * this.mult, p.y * this.mult), 0, 1, 90, 180);
-        } else {
-          angle = map(noise(p.x * this.mult, p.y * this.mult), 0, 1, 0, 90);
-        }
-        // let angle = p.heading();
-        this.points[i].add(createVector(cos(angle), sin(angle)));
+        angle = map(
+          noise(p.p.x * this.mult, p.p.y * this.mult),
+          0,
+          1,
+          this.points[i].heading - 1,
+          this.points[i].heading + 1
+        );
+        let tp = p5.Vector.fromAngle(angle);
+        this.points[i].p.add(tp.mult(1.3));
         if (
-          this.points[i].x > this.size ||
-          this.points[i].y > this.size ||
-          this.points[i].x < 0 ||
-          this.points[i].y < 0
+          this.points[i].p.x > this.size ||
+          this.points[i].p.y > this.size ||
+          this.points[i].p.x < 0 ||
+          this.points[i].p.y < 0
         ) {
-          console.log(this.points[i].x, this.points[i].y, "outside");
           break;
         }
-        circle(this.points[i].x, this.points[i].y, 1);
-        // }
+        curveVertex(this.points[i].p.x, this.points[i].p.y, 1);
+        endShape();
       }
     }
   }
--- genuary-2022-6-friends-style/sketch.js
+++ genuary-2022-6-friends-style/sketch.js
@@ -1,16 +1,25 @@
 function setup() {
   createCanvas(400, 400);
   noLoop();
-  // angleMode(DEGREES);
+  angleMode(DEGREES);
 }
 
 function draw() {
   let p = createVector(0, 100);
 
   translate(width / 2, height / 2);
-  p.setHeading(TWO_PI);
-  circle(p.x, p.y, 3);
-  line(0, 0, p.x, p.y);
-  console.log(p.heading());
-  arc(0, 0, 200, 200, 0, 15);
+  // p.setHeading(TWO_PI);
+  // circle(p.x, p.y, 3);
+  // line(0, 0, p.x, p.y);
+  // console.log(p.heading());
+  // arc(0, 0, 200, 200, 0, 15);
+
+  let points = [];
+  let radius = 20;
+  for (let i = 0; i < 360; i += 15) {
+    let np = createVector(0, 1);
+    np.setHeading(i);
+    np.mult(radius);
+    point(np.x, np.y);
+  }
 }
