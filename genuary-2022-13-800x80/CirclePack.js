f841bd3884666587265ef58411b08c9dbe7c84bd
jkenzer <jkenzer@gmail.com>
2022-01-13 19:05:36 -0700

    Day 13

---
+++ genuary-2022-13-800x80/CirclePack.js
@@ -0,0 +1,56 @@
+class CirclePack {
+  constructor(x, y, myWidth, myHeight, seedCircles) {
+    this.minR = 1;
+    this.maxR = 25;
+    this.allCircles = seedCircles;
+    this.numCircles = 300000;
+    let c;
+    let counter = 0;
+
+    for (let i = 0; i < this.numCircles; i++) {
+      c = {
+        x: random(this.minR + x, myWidth + x - this.minR),
+        y: random(this.minR + y, myHeight + y - this.minR),
+        r: this.minR,
+      };
+      if (this.isValid(c)) {
+        while (this.isValid(c) && c.r < this.maxR) {
+          c.r++;
+        }
+        this.allCircles.push(c);
+      }
+      counter++;
+      if (counter > 200000) {
+        console.log(counter);
+        break;
+      }
+    }
+  }
+  draw() {
+    this.allCircles.forEach((c) => {
+      if (c.r >= this.maxR * 0.5) {
+        strokeWeight(0);
+      } else {
+        strokeWeight(1);
+      }
+      circle(c.x, c.y, c.r * 2);
+    });
+  }
+  isValid(currentC) {
+    if (
+      currentC.x - currentC.r + 2 < 0 ||
+      currentC.x + currentC.r + 2 > width ||
+      currentC.y - currentC.r + 2 < 0 ||
+      currentC.y + currentC.r + 2 > height
+    ) {
+      return false;
+    }
+    for (let i = 0; i < this.allCircles.length; i++) {
+      const c = this.allCircles[i];
+      if (dist(currentC.x, currentC.y, c.x, c.y) < c.r + currentC.r + 2) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
--- genuary-2022-13-800x80/sketch.js
+++ genuary-2022-13-800x80/sketch.js
@@ -1,5 +1,35 @@
+let cp;
 function setup() {
   createCanvas(800, 80);
+  let seedR = 7;
+  let seedCircles = [];
+  // for (let i = seedR; i < width; i += seedR * 1.5) {
+  //   seedCircles.push({
+  //     x: i,
+  //     y: height / 2,
+  //     r: seedR,
+  //   });
+  // }
+  for (let i = seedR; i < width; i += seedR * 1.5) {
+    seedCircles.push({
+      x: i,
+      y: seedR,
+      r: seedR,
+    });
+  }
+  for (let i = seedR; i < width; i += seedR * 1.5) {
+    seedCircles.push({
+      x: i,
+      y: height - seedR,
+      r: seedR,
+    });
+  }
+  cp = new CirclePack(0, 0, 800, 80, seedCircles);
+  noLoop();
+  noFill();
 }
 
-function draw() {}
+function draw() {
+  // rect(0, 0, 800, 80);
+  cp.draw();
+}
